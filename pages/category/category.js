99353e29a96d623dc3abd65c9ed0240cb6ef4456
JackWang <wanghuanbing@fengbangleasing.com>
2020-05-07 10:48:24 +0800

    商品分类页面

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,6 +1,28 @@
-Page({
-    data: {},
-    onLoad: function (options) {
+import http from "../../utils/httpRequest";
 
+Page({
+    data: {
+        list: [],
+        rightList: [],
+        tapIndex: 0,
+        rightToTop: 0
+    },
+    onLoad: function (options) {
+        http.get("categories")
+            .then(res => {
+                this.setData({
+                    list: res,
+                    rightList: res[this.data.tapIndex].children
+                })
+            })
+            .catch();
+    },
+    leftCategoryTap(e) {
+        let {index} = e.mark;
+        this.setData({
+            tapIndex: index,
+            rightList: this.data.list[index].children,
+            rightToTop: 0
+        })
     }
 });
